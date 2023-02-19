d6eaca29764b9b3019217f6ff7f798ff3967905f
Opium1715 <284067302@qq.com>
2023-02-19 22:03:53 +0800

    test

--- src/test/java/CategoryServiceTest.java
+++ src/test/java/CategoryServiceTest.java
@@ -1,16 +1,20 @@
+import com.springboot.zq.MyApplication;
 import com.springboot.zq.servicce.CategoryService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
-
-@SpringBootTest
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = MyApplication.class)
 public class CategoryServiceTest {
     @Autowired
     private CategoryService categoryService;
 
+    @Test
+    public void testForloadData(){
+        categoryService.getAllCategory(1).getList().forEach(System.out::println);
+    }
 
 }
