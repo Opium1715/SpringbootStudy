cb19f0c136f919c3caa230e03c4973746080483e
Opium1715 <284067302@qq.com>
2023-03-04 10:50:38 +0800

    3-4 1050

---
+++ src/main/java/com/springboot/zq/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.springboot.zq.mapper;
+
+import com.springboot.zq.pojo.User;
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    List<User> selectAll();
+
+    int updateByPrimaryKey(User record);
+}
